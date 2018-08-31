1f1723b20f8c709eca96b709a8bc04207574fcac
qianxuebing <you@example.com>
2018-08-31 19:39:25 +0800

    other

---
+++ MOBILE/JS/rem.js
@@ -0,0 +1,36 @@
+// (function (doc, win) {
+// var docEl = doc.documentElement,
+// resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+// recalc = function () {
+// var clientWidth = docEl.clientWidth;
+// if (!clientWidth) return;
+// // 默认设计图为640的情况下1rem=100px；根据自己需求修改
+// if(clientWidth>=640){
+// docEl.style.fontSize = '100px';
+// }else{
+// docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
+// }
+// };
+//  
+// if (!doc.addEventListener) return;
+// win.addEventListener(resizeEvt, recalc, false);
+// doc.addEventListener('DOMContentLoaded', recalc, false);
+// })(document, window);
+
+(function (doc, win) {
+        var docEl = doc.documentElement,
+            resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+            recalc = function () {
+                var clientWidth = docEl.clientWidth;
+                if (!clientWidth) return;
+                if(clientWidth>=640){
+                    docEl.style.fontSize = '100px';
+                }else{
+                    docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
+                }
+            };
+
+        if (!doc.addEventListener) return;
+        win.addEventListener(resizeEvt, recalc, false);
+        doc.addEventListener('DOMContentLoaded', recalc, false);
+    })(document, window);
